e6dd00adc2e7413b691c0314e4fea8c20dd47dd0
karan sivarat <bomb0069@gmail.com>
2021-01-16 14:01:42 +0700

    [Added]: init slice and make a slice

--- workshop1/array/slice.go
+++ workshop1/array/slice.go
@@ -3,8 +3,18 @@ package main
 import "fmt"
 
 func main() {
+	// Array
 	var colors = [5]string{"Red", "Blue", "Green", "Yellow", "Black"}
+
+	// Slice
+	var colorSlice = []string{"Red", "Blue", "Green", "Yellow", "Black"}
+
+	// Make
+	var colorMake = make([]string, 5)
+
 	fmt.Println("Colors = ", colors)
+	fmt.Println("Colors = ", colorSlice)
+	fmt.Println("Colors = ", colorMake)
 
 	a := colors[0:2]
 	a = append(a, "XXX")
