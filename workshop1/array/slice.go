9052fca6f5307064f798ce50edc5d62af659ef72
karan sivarat <bomb0069@gmail.com>
2021-01-16 13:54:37 +0700

    [Added]: append top Slice

--- workshop1/array/slice.go
+++ workshop1/array/slice.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var colors = [3]string{"Red", "Blue", "Green"}
+	var colors = [5]string{"Red", "Blue", "Green", "Yellow", "Black"}
 	fmt.Println("Colors = ", colors)
 
 	a := colors[0:2]
+	a = append(a, "XXX")
 	fmt.Println("A = ", a)
 	a[0] = "New"
 	fmt.Println("Colors,a = ", colors, a)
