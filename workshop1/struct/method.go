37f04c50b8dcaddb834d21551970ca2cde48b822
karan sivarat <bomb0069@gmail.com>
2021-01-16 15:32:07 +0700

    [Added]: Method of Struct and Overwrite Method in Go (for embeded)

--- workshop1/struct/method.go
+++ workshop1/struct/method.go
@@ -4,17 +4,25 @@ import (
 	"fmt"
 )
 
-type User2 struct {
-	Id   int
+type User struct {
 	Name string
 }
 
+type User2 struct {
+	Id int
+	User
+}
+
+func (user User) printName() {
+	fmt.Println("User := ", user.Name)
+}
+
 func (user User2) printName() {
-	fmt.Println(user.Name)
+	fmt.Println("User 2 := ", user.Name)
 }
 
 func main() {
-	u2 := User2{2, "karan"}
+	u2 := User2{2, User{"karan"}}
 	u2.printName()
 
 }
