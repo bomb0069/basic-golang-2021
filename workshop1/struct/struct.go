0bbe1e947b0ef90550442c3984e3c3bc4fdefd13
karan sivarat <bomb0069@gmail.com>
2021-01-16 15:06:54 +0700

    [Added]: New Struct

--- workshop1/struct/struct.go
+++ workshop1/struct/struct.go
@@ -4,15 +4,21 @@ import (
 	"fmt"
 )
 
-type user struct {
-	id   int
-	name string
+type User struct {
+	Id   int
+	Name string
+}
+
+func New(id int, name string) User {
+	return User{1, "bomb0069"}
 }
 
 func main() {
-	u1 := user{1, "bomb0069"}
-	u2 := user{id: 2, name: "karan"}
-	u1.name = "XXX"
+	u1 := User{1, "bomb0069"}
+	u2 := User{Id: 2, Name: "karan"}
+	u3 := New(1, "bomb0069")
+	u1.Name = "XXX"
 	fmt.Printf("%v", u1)
 	fmt.Printf("%+v", u2)
+	fmt.Printf("%+v", u3)
 }
